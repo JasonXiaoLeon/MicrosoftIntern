9360dfd91303f889e922637c413fd87e1d30052e
JasonXiaoLeon <79917099+JasonXiaoLeon@users.noreply.github.com>
2024-01-05 23:43:09 +1000

    Update testConnection.java with Finally

--- microsoft app/SocialApp/src/JDBC/testConnection.java
+++ microsoft app/SocialApp/src/JDBC/testConnection.java
@@ -11,36 +11,44 @@ public class testConnection {
     private static final String PASSWORD = "123456";
     
     public static void main(String[] args) {
-    // 尝试建立数据库连接
-    try {
-        // 注册MySQL JDBC驱动程序
-        Class.forName("com.mysql.cj.jdbc.Driver");
-
-        // 建立连接
-        Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
-
-        // 如果连接成功，打印成功消息
-        if (connection != null) {
-            System.out.println("成功连接到MySQL数据库！");
-            // 在此处执行数据库操作
-
-            // 关闭连接
-            connection.close();
-        }
-	    } catch (ClassNotFoundException e) {
-	        System.err.println("无法找到MySQL JDBC驱动程序");
-	        e.printStackTrace();
-    } catch (SQLException e) {
-        System.err.println("数据库连接失败");
-        e.printStackTrace();
-    }
-}
-    // 获取数据库连接的方法
-    public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
-    }
-    public Statement createStatement() throws SQLException {
-        Connection connection = getConnection();
-        return connection.createStatement();
-    }
+        Connection connection = null;
+	    // 尝试建立数据库连接
+	    try {
+	        // 注册MySQL JDBC驱动程序
+	        Class.forName("com.mysql.cj.jdbc.Driver");
+	
+	        // 建立连接
+	        connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+	
+	        // 如果连接成功，打印成功消息
+		        if (connection != null) {
+		            System.out.println("成功连接到MySQL数据库！");
+		            // 在此处执行数据库操作
+		        }
+		    } catch (ClassNotFoundException e) {
+		        System.err.println("无法找到MySQL JDBC驱动程序");
+		        e.printStackTrace();
+		    } catch (SQLException e) {
+		        System.err.println("数据库连接失败");
+		        e.printStackTrace();
+		    }finally {
+	            // 在 finally 块中关闭连接
+	            if (connection != null) {
+	                try {
+	                    connection.close();
+	                    System.out.println("数据库连接已关闭！");
+	                } catch (SQLException e) {
+	                    e.printStackTrace();
+	                }
+	            }
+		    }
+	}
+	    // 获取数据库连接的方法
+	    public Connection getConnection() throws SQLException {
+	        return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+	    }
+	    public Statement createStatement() throws SQLException {
+	        Connection connection = getConnection();
+	        return connection.createStatement();
+	    }
 }
