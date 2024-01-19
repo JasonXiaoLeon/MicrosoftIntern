80a4ca48965ee034daaeb2b1578eeb4e03ecb81e
JasonXiaoLeon <79917099+JasonXiaoLeon@users.noreply.github.com>
2024-01-19 17:06:53 +1000

    Delete microsoft app/demo/src directory

--- microsoft app/demo/src/main/java/com/example/demo/week2/bean/PressDAO.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.week2.bean;
-
-public interface PressDAO {
-    public void test();
-}
--- microsoft app/demo/src/main/java/com/example/demo/week2/bean/PressDAOImpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.week2.bean;
-
-public class PressDAOImpl implements PressDAO{
-    @Override
-    public void test()  {
-        System.out.println("tested");
-    }
-}
